bfd278256762534751176a52da0bfc6affb2c96c
Ergün Elvan Bilsel <elvan.bilsel@turksat.com.tr>
2025-09-02 09:58:14 +0300

    README

---
+++ sangfor_sdk/__ini__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Sangfor API SDK
+~~~~~~~~~~~~~~~
+
+A Python wrapper for the Sangfor SCP Open-API.
+
+:copyright: (c) 2025 by Ergün Elvan Bilsel
+:license: MIT, see LICENSE for more details.
+"""
+
+# This makes the SangforSDKClient class directly importable from the package.
+# e.g., from sangfor_sdk import SangforSDKClient
+from .client import SangforSDKClient
+
+__version__ = "0.0.1"
